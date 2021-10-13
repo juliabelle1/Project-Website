0d23117c88bcfa5917ce8a51230fa018f157d364
Julia <julia.zhmudyk@gmail.com>
2021-10-13 22:52:43 +0200

    finished commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const button = document.querySelector('.navbar-toggler');
 const menu = document.querySelector('.navbar-appear');
 const close = document.querySelector('.close');
 const menyDark = document.querySelector('.bg-dark');
+const order = document.querySelector('.order');
 
 button.addEventListener('click',() =>{
     menu.style.display = 'flex'
@@ -24,5 +25,19 @@ window.onscroll = () => {
     }
 }
 
+order.addEventListener('mouseover',() =>{
+    Swal.fire({
+        title: 'Veckans erbjudande!!!',
+        text: 'Köp två rätter och få en dryck gratis!',
+        imageUrl: 'https://images.unsplash.com/photo-1579684947550-22e945225d9a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80',
+        imageWidth: 350,
+        imageHeight: 300,
+        imageAlt: 'Custom image',
+        background: '#ffdd93',
+        confirmButtonText:'<a href="menu.html#pasta">Gör beställning</a>',
+        confirmButtonColor: 'rgba(29, 28, 28, 0.9)',
+
+})
 
 
+})
